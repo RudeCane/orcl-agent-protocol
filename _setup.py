3d1b94ba8a078cd4c33646f8597b3d8df8930279
RudeCane <dawgcommunicationsllc@gmail.com>
2026-03-18 15:23:33 -0400

    Updated README with full installation guide, all exchanges

--- _setup.py
+++
@@ -1,5 +0,0 @@
-import os
-os.makedirs("orderbook", exist_ok=True)
-with open("orderbook/__init__.py", "w") as f:
-    f.write("from orderbook.fetcher import OrderBookFetcher, OrderBook, orderbook_fetcher\n")
-print("Created __init__.py")
